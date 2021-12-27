7e2d05fee86e136e04877000b8e0b01d4ea38b1e
xoste49 <xoste49@gmail.com>
2021-12-27 13:53:45 +0300

    Add models Recipe and Tag

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """
+    Рецепт
+    Рецепт должен описываться такими полями:
+    Автор публикации (пользователь).
+    Название.
+    Картинка.
+    Текстовое описание.
+    Ингредиенты: продукты для приготовления блюда по рецепту. Множественное поле, выбор из предустановленного списка, с указанием количества и единицы измерения.
+    Тег (можно установить несколько тегов на один рецепт, выбор из предустановленных).
+    Время приготовления в минутах.
+    Все поля обязательны для заполнения.
+    """
+    # author
+    title = models.CharField(
+        verbose_name='Название рецепта',
+        default='',
+        max_length=100,
+    )
+    # image
+    description = models.CharField(
+        verbose_name='Описание рецепта',
+        blank=True,
+        null=False,
+        default='',
+        max_length=3000,
+    )
+    # ingredients
+    # tag
+    # cookingtime
+
+
+class Tag(models.Model):
+    """
+    Тег
+    Тег должен описываться такими полями:
+    Название
+    Цветовой HEX-код (например, #49B64E)
+    Slug
+    Все поля обязательны для заполнения и уникальны
+    """
+    name = models.CharField(
+        verbose_name='Имя тега',
+        max_length=100,
+    )
+    #color
+    #slug
+    pass
