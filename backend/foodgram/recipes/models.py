4d28522274d2f4162d99ade686947275570396b8
xoste49 <xoste49@gmail.com>
2022-01-07 21:58:49 +0300

    Update models.py

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,10 +1,51 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    """
+    Тег
+    Все поля обязательны для заполнения и уникальны
+    """
+    name = models.CharField(
+        verbose_name='Имя тега',
+        max_length=100,
+        unique=True
+    )
+    color = models.CharField(
+        verbose_name='Цвет',
+        help_text='Цвет в формате HEX. Пример: #RRGGBB',
+        max_length=7,
+        unique=True,
+    )
+    slug = models.SlugField(
+        verbose_name='Slug Tag',
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название ингредиента',
+        max_length=100,
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=20,
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
     """
     Рецепт
-    Рецепт должен описываться такими полями:
     Автор публикации (пользователь).
     Название.
     Картинка.
@@ -14,38 +55,73 @@ class Recipe(models.Model):
     Время приготовления в минутах.
     Все поля обязательны для заполнения.
     """
-    # author
-    title = models.CharField(
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        db_column='author',
+        verbose_name='Автор',
+    )
+    name = models.CharField(
         verbose_name='Название рецепта',
-        default='',
         max_length=100,
     )
-    # image
-    description = models.CharField(
+    image = models.ImageField(upload_to='images/')
+    text = models.CharField(
         verbose_name='Описание рецепта',
         blank=True,
         null=False,
-        default='',
         max_length=3000,
     )
-    # ingredients
-    # tag
-    # cookingtime
-
-
-class Tag(models.Model):
-    """
-    Тег
-    Тег должен описываться такими полями:
-    Название
-    Цветовой HEX-код (например, #49B64E)
-    Slug
-    Все поля обязательны для заполнения и уникальны
-    """
-    name = models.CharField(
-        verbose_name='Имя тега',
-        max_length=100,
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингредиенты',
     )
-    #color
-    #slug
-    pass
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тэги',
+    )
+    cooking_time = models.IntegerField(
+        verbose_name='Время приготовления в минутах.',
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class Subscription(models.Model):
+    """
+    user — ссылка на объект пользователя, который подписывается.
+    author — ссылка на объект пользователя, на которого подписываются,
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="subscriber",
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="following",
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+    )
