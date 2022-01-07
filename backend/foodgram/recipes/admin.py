1049a5dbc8540acec33e749931f45d607189674a
xoste49 <xoste49@gmail.com>
2022-01-07 21:58:15 +0300

    Add RecipeAdmin, TagAdmin

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Tag, Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'text', 'cooking_time')
+    search_fields = ('name',)
+    list_filter = ('id', 'name', 'cooking_time')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color')
+    search_fields = ('name',)
+    list_filter = ('id', 'name')
+    empty_value_display = '-пусто-'
